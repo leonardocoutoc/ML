34396ddb52219225942ba5bba0bef0e592936426
vboxuser <vboxuser@PYTHON.myguest.virtualbox.org>
2023-04-02 13:29:56 -0300

    version update 1.3

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import owners_m
 import databases_m
 import notifica_m
-import time
+#import time
 
-i=1
-j=15
-while i <= 15:
-    print("Waiting for MySQL service, please wait... ", j,"s" )
-    time.sleep(1)
-    i=i+1
-    j=j-1
+# i=1
+# j=15
+# while i <= 15:
+#     print("Waiting for MySQL service, please wait... ", j,"s" )
+#     time.sleep(1)
+#     i=i+1
+#     j=j-1
 
 owners_m.get_owners()
 databases_m.get_database()
