d01a73c1e3afc9cd59d36219c95e063737e3227c
vboxuser <vboxuser@PYTHON.myguest.virtualbox.org>
2023-04-02 12:29:03 -0300

    version update 1.2

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM python:alpine3.17
 WORKDIR /opt/ml_app/
-RUN apk add git && git clone https://github.com/leonardocoutoc/ML
+RUN apk add git mysql-client mariadb-connector-c && git clone https://github.com/leonardocoutoc/ML
 RUN python3 -m venv ML
 RUN pip install mysql-connector-python
--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-import p_create_database_m
 import owners_m
 import databases_m
 import notifica_m
+import time
 
-p_create_database_m.create_database()
+i=1
+j=15
+while i <= 15:
+    print("Waiting for MySQL service, please wait... ", j,"s" )
+    time.sleep(1)
+    i=i+1
+    j=j-1
+
 owners_m.get_owners()
 databases_m.get_database()
 notifica_m.send_mail_to_managers()
